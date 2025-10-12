62a062ae8bedf845b0d965f003a01ea67594ebcb
zeaforx <ajibcaleb@gmail.com>
2025-10-12 14:50:01 +0100

    feat: Add axios for API requests and enhance XtendedMuiGrid component
    - Added axios dependency to package.json for making HTTP requests.
    - Implemented request utility using axios for GET, POST, DELETE, and PATCH operations in XtendedMuiGrid.
    - Enhanced filtering functionality with local state management for filters.
    - Introduced loading indicators and pagination controls, including a jump-to-page feature.
    - Updated CustomToolbar to manage filter configurations and export options.
    - Improved error handling for API responses and added support for exporting data in CSV and Excel formats.
    - Refactored filter management to support dynamic filter options based on filterMap.
    - Updated types in types.ts to accommodate new props and functionalities.

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,25 +1,33 @@
 import type { GridColDef } from "@mui/x-data-grid";
-
+import { JSX } from "react/jsx-runtime";
 
 export const FETCH_MODE = {
   GET: "get",
+
   POST: "post",
 } as const;
 
 export type FilterItem = {
   field: string;
+
   operator: string;
+
   value: string | number;
 };
 
 export type FilterPayload = {
   filter?: {
     items: FilterItem[];
+
     logicOperator?: "and" | "or";
   };
+
   sort?: { field: string; sort: "asc" | "desc" }[];
+
   limit?: number;
+
   offset?: number;
+
   [key: string]: any; // index signature
 };
 
@@ -27,12 +35,30 @@ export type FilterPayloadDef = FilterPayload | URLSearchParams;
 
 export type CustomDataGridDef = {
   columns: GridColDef[];
+
+  externalLoading?: Boolean;
+
+  filterMap?: Record<string, any[]>;
+
   defaultFilter: Array<Record<string, any>>;
+
+  getRowId?: (row: any) => string;
+
   handleFilterChange: (payload: FilterPayloadDef) => void;
+
+  slotProps?: Record<string, any>;
+
   gridData: Record<string, any>;
+
+  renderRowMenu?: (business: any, onClose: () => void) => JSX.Element;
+
   handleExport?: (payload: FilterPayloadDef, fileType: "csv" | "excel") => void;
+
   csvExportUrl?: string;
+
   excelExportUrl?: string;
+
   exportFileName?: string;
+
   fetchMode?: (typeof FETCH_MODE)[keyof typeof FETCH_MODE];
 };
